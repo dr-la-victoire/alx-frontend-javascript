b800ac91ee2baf5dd970b3450285e9a4b665e7d2
Victory Amadi <lavictoire633@gmail.com>
2024-05-29 15:16:50 +0100

    ...

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
-  }
-
-  return array;
+  return array.map((value) => appendString + value);
 }
