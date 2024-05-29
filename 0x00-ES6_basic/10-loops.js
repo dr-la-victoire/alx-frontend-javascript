279d80a396146af7da896c1b827d336aa9726dce
Victory Amadi <lavictoire633@gmail.com>
2024-05-29 15:22:07 +0100

    fix function

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  for (const idx in array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
 }
