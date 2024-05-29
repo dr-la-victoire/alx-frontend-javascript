02c79b88e552335b12996eab592f2d6b58a6bbe2
Victory Amadi <lavictoire633@gmail.com>
2024-05-29 13:20:02 +0100

    fix the catch block

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,6 +2,6 @@ export default function handleResponseFromAPI(promise) {
   const response = { status: 200, body: 'success' }
   return promise
     .then(() => response)
-    .catch((error) => { error })
+    .catch((error) => error)
     .finally(() => console.log('Got a response from the API'));
 }
