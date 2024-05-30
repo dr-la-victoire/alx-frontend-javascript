fb0efbf883236ee915ec1b4c4b93a90adf82d3c9
Victory Amadi <lavictoire633@gmail.com>
2024-05-30 12:18:14 +0100

    creating an instance of a class: forgot to add the new keyword

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,11 @@
-import { ClassRoom } from './0-classroom';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [ClassRoom(19), ClassRoom(20), ClassRoom(34)];
+  const sizeOne = new ClassRoom(19);
+  const sizeTwo = new ClassRoom(20);
+  const sizeThree = new ClassRoom(34);
+
+  const sizeArr = [sizeOne, sizeTwo, sizeThree];
+
+  return sizeArr;
 }
