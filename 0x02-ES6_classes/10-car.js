48a0f98df7957afd763cc78ebda34ad5fc55a312
Victory Amadi <lavictoire633@gmail.com>
2024-05-30 20:52:42 +0100

    Tasks 8 & 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,20 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    } else if (typeof motor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    } else if (typeof color !== 'string') {
+      throw new TypeError('Color must be a string');
+    } else {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  }
+
+  // Methods
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
