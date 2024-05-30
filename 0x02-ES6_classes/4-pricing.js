e2e2466e7fdb9c5d9e4640a21fd20a7252e3cef1
Victory Amadi <lavictoire633@gmail.com>
2024-05-30 14:46:57 +0100

    more classes in es6

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,50 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be instance of Currency');
+    } else {
+      this._amount = amount;
+      this._currency = currency;
+    }
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+  set amount(theAmount) {
+    if (typeof theAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = theAmount;
+  }
+
+  set currency(theCurrency) {
+    if (!(theCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be instance of Currency');
+    }
+    this._currency = theCurrency;
+  }
+
+  // Methods
+  displayFullPrice() {
+    const firstAttr = this._amount;
+    const secondAttr = this._currency.displayFullCurrency();
+
+    return `${firstAttr} (${secondAttr})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
