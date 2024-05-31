959be522c898deec3a3a19b8c73ba6efddbeedba
Victory Amadi <lavictoire633@gmail.com>
2024-05-31 16:48:58 +0100

    async/await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
+  let uploadPhotoResponse;
+  let createUserResponse;
   try {
-    let uploadPhotoResponse = await uploadPhoto();
-    let createUserResponse = await createUser();
+    uploadPhotoResponse = await uploadPhoto();
+    createUserResponse = await createUser();
   } catch (error) {
     uploadPhotoResponse = null;
     createUserResponse = null;
   }
-
   const obj = { photo: uploadPhotoResponse, user: createUserResponse };
 
   return obj;
