0b7c3f9eef534e6e9d3585252a5502e4bc3b86d0
Victory Amadi <lavictoire633@gmail.com>
2024-05-31 20:24:40 +0100

    fix styling errors

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,8 +3,8 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('Sqft must be a number');
     } else if (
-      this.constructor !== Building &&
-      typeof this.evacuationWarningMessage !== 'function'
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
     ) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     } else {
