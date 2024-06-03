17feea810d38977f425d17a4508c15663dc989a6
Victory Amadi <lavictoire633@gmail.com>
2024-06-03 20:46:33 +0100

    function that checks whether the elements of an array are all in a set

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,15 @@
+export default function hasValuesFromArray(set, array) {
+  const arrayLength = array.length;
+  let num = 0;
+  for (const value of array) {
+    if (set.has(value)) {
+      num += 1;
+    }
+  }
+
+  if (num === arrayLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
