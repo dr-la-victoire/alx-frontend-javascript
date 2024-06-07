5e7cd959e1d56619831769df9102f11c7f41703c
La Victoire <lavictoire633@gmail.com>
2024-06-07 14:42:23 +0000

    making a function interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// The Teacher Interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -7,10 +8,19 @@ interface Teacher {
   [key: string]: any;
   }
 
+
+// The Director Interface extends the Teacher Interface
 interface Directors extends Teacher {
   numberOfReports: number;
-}
+  }
 
+
+ // The Function Interface: Prints a teacher's name
+ interface printTeacherFunction {
+   (firstName: string, lastName: string): string;
+ }
+
+ // Creating a Teacher
   const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -20,3 +30,10 @@ interface Directors extends Teacher {
 };
 
 console.log(teacher3);
+
+// Function that prints a teacher's initial and lastname
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const first = firstName.charAt(0);
+
+  return `${first}. ${lastName}`;
+}
