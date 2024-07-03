c55d5ccfb1e71b1df06a45e5ab27a8becc53938f
La Victoire <doctorvictory116@gmail.com>
2024-07-03 19:29:07 +0000

    compiling the ts file for task_1

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,3 +1,4 @@
+// Creating a Teacher
 var teacher3 = {
     firstName: 'John',
     fullTimeEmployee: false,
@@ -6,3 +7,22 @@ var teacher3 = {
     contract: false,
 };
 console.log(teacher3);
+// Function that prints a teacher's initial and lastname
+var printTeacher = function (firstName, lastName) {
+    var first = firstName.charAt(0);
+    return "".concat(first, ". ").concat(lastName);
+};
+// Writing the Student class
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
